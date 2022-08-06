ef1376f6c2b124b47cc7e4103016a8eb0b8f62ad
surendra-ksr <reddysurendra106@gmail.com>
2022-08-06 22:29:46 +0530

    added some finctions, cleaning code

--- store_quotes.py
+++ store_quotes.py
@@ -8,29 +8,52 @@ print("Connected to database")
 
 
 
-def creating_quotes_tags_authors_table():
+def creating_quotes_table():
     create_quotes_table_command = """CREATE TABLE quotes (
-    quote_id INTEGER PRIMARY KEY,
+    quote_id INTEGER PRIMARY KEY AUTOINCREMENT,
     quote TEXT,
-    author VARCHAR(200));"""
+    author_id INTEGER,
+    FOREIGN KEY(author_id) REFERENCES authors(author_id) on DELETE CASCADE);"""
+    
+    cursor_object.execute("DROP TABLE IF EXISTS quotes")
+    cursor_object.execute(create_quotes_table_command)
 
+def creating_authors_table():
     create_authors_table_command = """CREATE TABLE authors (
-    name VARCHAR(200) PRIMARY KEY,
+    author_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(200) ,
     born TEXT,
     reference_url VARCHAR(250));"""
 
-    create_tags_table_command = """CREATE TABLE tags (
-    tag_id INTEGER PRIMARY KEY,
-    tag VARCHAR(100),
-    quote_id INTEGER,
-    FOREIGN KEY(quote_id) REFERENCES quotes(quote_id) on DELETE CASCADE );"""
-
-    cursor_object.execute(create_quotes_table_command)
+    cursor_object.execute("DROP TABLE IF EXISTS authors")
     cursor_object.execute(create_authors_table_command)
+
+def creating_tags_table():
+    create_tags_table_command = """CREATE TABLE tags (
+    tag_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tag VARCHAR(100) UNIQUE );"""
+
+    cursor_object.execute("DROP TABLE IF EXISTS tags")
     cursor_object.execute(create_tags_table_command)
 
-creating_quotes_tags_authors_table()
+def creating_quote_tag_table():
+    create_quote_tag_table_command = """CREATE TABLE quote_tag (
+    id INTEGER NOT NULL PRIMARY KEY,
+    tag_id INTEGER,
+    quote_id INTEGER,
+    FOREIGN KEY(tag_id) REFERENCES tags(tag_id) on DELETE CASCADE,
+    FOREIGN KEY(quote_id) REFERENCES quotes(quote_id) on DELETE CASCADE );"""
 
+    cursor_object.execute("DROP TABLE IF EXISTS quote_tag")
+    cursor_object.execute(create_quote_tag_table_command)
+
+def creating_quotes_tags_authors_table():
+    creating_quotes_table()
+    creating_authors_table()
+    creating_tags_table()
+    creating_quote_tag_table()
+
+creating_quotes_tags_authors_table()
 
 def get_json_data_from_json_file():
 
@@ -43,41 +66,45 @@ json_data = get_json_data_from_json_file()
 
 
 def insert_data_into_authors_table(authors_list):
-    insert_authors_command = "INSERT INTO authors VALUES(?,?,?);"
+    insert_authors_command = "INSERT OR IGNORE INTO authors(name,born,reference_url) VALUES(?,?,?);"
     for author in authors_list:
         name = author["name"]
         born = author["born"]
         reference = author["reference"]
         cursor_object.execute(insert_authors_command,(name,born,reference))
-        
+
+authors_list = json_data["authors"]
+insert_data_into_authors_table(authors_list)        
 
 def inserting_data_into_tables(json_data):
     quotes_list = json_data["quotes"]
     authors_list = json_data["authors"]
 
     #initialising quote_count and tag_count to create 'quote_id' and 'tag_id' in the tables
-    quote_count = 1
-    tag_count = 1
-    insert_quotes_command = "INSERT INTO quotes VALUES(?,?,?);"
-    insert_tags_command = "INSERT INTO tags VALUES(?,?,?);"
+    insert_quotes_command = "INSERT INTO quotes(quote,author_id) VALUES(?,(SELECT author_id FROM authors WHERE name=?));"
     
     #getting individual Quote and inserting it into 'quote' table
     for quote_object in quotes_list:
         quote = quote_object["quote"]
         author = quote_object["author"]
-        cursor_object.execute(insert_quotes_command,(quote_count,quote,author))
-   
-        for each in quote_object["tags"]:
-            cursor_object.execute(insert_tags_command,(tag_count,each,quote_count))
-            tag_count +=1
-        
-        quote_count +=1
+        tags = quote_object["tags"]
+        cursor_object.execute(insert_quotes_command,(quote,author))
 
-    insert_data_into_authors_table(authors_list)
+        inserting_in_tags_and_quote_tag_tables(tags,quote)
+
+def inserting_in_tags_and_quote_tag_tables(tags,quote):
+    insert_tags_command = "INSERT OR IGNORE INTO tags(tag) VALUES(?);"
+    insert_quote_tag_command = "INSERT INTO quote_tag(tag_id,quote_id) VALUES((SELECT tag_id FROM tags WHERE tag=?),(SELECT quote_id FROM quotes WHERE quote=?));"
+    for each in tags:
+        cursor_object.execute(insert_tags_command,(each,))
+        cursor_object.execute(insert_quote_tag_command,(each,quote))
 
 inserting_data_into_tables(json_data)
 
+authors_list = json_data["authors"]
+insert_data_into_authors_table(authors_list)     
 
 db_connection.commit()
 
 db_connection.close()
+print("completed")
