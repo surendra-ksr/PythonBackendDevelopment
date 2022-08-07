d0db441e4573ad9bb3c82d0399b12619f15b2e1a
surendra-ksr <reddysurendra106@gmail.com>
2022-08-07 12:31:38 +0530

    added some finctions, cleaning code

--- analyze_quotes.py
+++ analyze_quotes.py
@@ -10,13 +10,12 @@ def total_quotes_count():
     cursorObject.execute(getTotalQuotes_command)
     totalQuotes = cursorObject.fetchall()
     stringStatement = "Total no. of quotations on the website are :"
-    print("\n") #To get Line Space
     totalQuotes = stringStatement + str(totalQuotes[0][0])
     return totalQuotes
     
 # To get no. of quotations authored by the given author’s name with 'authorName' as parameter.
 def total_quotes_by(authorName):
-    getCountOfQuotesByAuthor_Command = """SELECT COUNT(*) FROM quotes WHERE author = ?""" 
+    getCountOfQuotesByAuthor_Command = """SELECT COUNT(*) FROM quotes NATURAL JOIN authors WHERE name= ?""" 
     cursorObject.execute(getCountOfQuotesByAuthor_Command,(authorName,)) # "authorName" is the parameter
     quotesCount = cursorObject.fetchall()
     resultStatement = "No. of quotations authored by the given {} : ".format(authorName)
@@ -25,7 +24,7 @@ def total_quotes_by(authorName):
 
 # To get Minimum, Maximum, and Average no. of tags on the quotations.
 def analyse_tags_count():
-    getAnalysisOfTags_Command = """SELECT MIN(tagCount), MAX(tagCount), ROUND(AVG(tagCount),2) FROM (SELECT COUNT(tag_id) AS tagCount FROM tags GROUP BY quote_id)""" 
+    getAnalysisOfTags_Command = """SELECT MIN(tagCount), MAX(tagCount), ROUND(AVG(tagCount),2) FROM (SELECT COUNT(tag_id) AS tagCount FROM quote_tag GROUP BY quote_id)""" 
     cursorObject.execute(getAnalysisOfTags_Command)
     tagsAnalysis = cursorObject.fetchall()
     strings = ["Minimum no. of tags on the quotations: ", "Maximun no. of tags on the quotations: ","Average no. of tags on the quotations: "]
@@ -37,7 +36,7 @@ def analyse_tags_count():
 """To get top N(with 'N' as parameter) authors who authored 
     the maximum number of quotations sorted in descending order of no. of quotes"""        
 def get_top_n_no_of_authors(N):
-    getAnalysisOfTags_Command = """SELECT author, COUNT(author) AS countQuotes FROM quotes GROUP BY author ORDER BY countQuotes DESC LIMIT ? """ 
+    getAnalysisOfTags_Command = """SELECT name AS author FROM quotes NATURAL JOIN authors GROUP BY author_id ORDER BY COUNT(name) DESC LIMIT ? """ 
     cursorObject.execute(getAnalysisOfTags_Command,(N,))  # "N" is the parameter
     authors = cursorObject.fetchall()
     print("Top {} authors who authored the maximum number of quotations are : ".format(N))
@@ -47,12 +46,12 @@ def get_top_n_no_of_authors(N):
     return topAuthorsList   
 
 def get_tags_of_author(author):
-    getAnalysisOfTags_Command = """SELECT tag FROM quotes LEFT JOIN tags ON quotes.quote_id = tags.quote_id WHERE author=?""" 
+    getAnalysisOfTags_Command = """SELECT tag FROM (tags NATURAL JOIN quote_tag) NATURAL JOIN (quotes NATURAL JOIN authors) WHERE name=?""" 
     cursorObject.execute(getAnalysisOfTags_Command,(author,))   # "author" is the parameter
     tags = cursorObject.fetchall()
     print("Tags related to author {} are : ".format(author))
     tagsOfAuthor = []
-    for i in tags:
+    for i in tags: 
         tagsOfAuthor.append(i[0])
     return tagsOfAuthor
 
@@ -62,7 +61,6 @@ def get_link_of_an_author(author):
     link = cursorObject.fetchall()
     exactLink = link[0][0]
     return exactLink
-
 
 total_quotes_count()
 
@@ -76,8 +74,9 @@ get_tags_of_author("Albert Einstein")
 
 get_link_of_an_author("Albert Einstein")
 
-print(get_link_of_an_author("Albert Einstein"))
+
 print(get_tags_of_author("Albert Einstein"))
+print(get_link_of_an_author("Albert Einstein"))
 print(get_top_n_no_of_authors(5))
 print(analyse_tags_count())
 print(total_quotes_by("Albert Einstein"))
